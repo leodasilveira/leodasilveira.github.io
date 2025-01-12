991077a03483d131ad67340cbddeb1e0eb0a52ff
leodasilveira <129402180+leodasilveira@users.noreply.github.com>
2025-01-12 17:02:57 -0300

    Update nfc.js

--- app/nfc.js
+++ app/nfc.js
@@ -11,6 +11,7 @@ async function readTag() {
       ndef.onreading = event => {
         const decoder = new TextDecoder();
         for (const record of event.message.records) {
+          console.log(record);
           consoleLog("ID da Tag: " + record.id);
           // consoleLog("Record type:  " + record.recordType);
           // consoleLog("MIME type:    " + record.mediaType);
